dcf45f31a04676fdf9c99a2107f0dff2ef4efe1e
Rohan Patel <rspatel014@gmail.com>
2021-02-04 14:17:44 -0800

    now hides the start button and replaces with reset once the timer has started

--- source/app.js
+++ source/app.js
@@ -1,40 +1,63 @@
 
 /**
- * Phases: work, small_break, long_break (lowercase)
+ * Phases: idle, work, small_break, long_break (lowercase)
  * 
- * W = work, b = small break, B = big break
+ * i = idle, W = work, b = small break, B = big break
  * 
- * W-b-W-b-W-b-W-B (1 cycle)
- * 0-1-0-1-0-1-0-2
+ * i-W-b-W-b-W-b-W-B (1 cycle)
+ * i-0-1-0-1-0-1-0-2
  */
 
-var tasks = [];
-var task;
-var phase = "work";
-var tasksDone = 0;
+let tasks = [];
+let task;
+let phase = "idle";
+let tasksDone = 0;
+
+/**
+ * Break lengths
+ */
+let work_length = 5;            // work time (seconds)   15 mins (900)
+let small_break_length = 7;     // small break time      5 mins  (300)
+let big_break_length = 10;      // big break time        25 mins (1500)
 
 /**
  * Start the timer and update the timer every second
  */
 function start() {
-    let secondsRemaining = setTimeRemaining();
     phase = "work";
+    let secondsRemaining = setTimeRemaining();
     tasksDone = 0;
+    document.getElementById("reset").style.display = 'block';
+    document.getElementById('start').style.display = 'none';
 
-    let timer = setInterval(function () {
-        // once all the tasks have ended, clear the timer
-        if (tasks.length == 0) clearInterval(timer);
+    if (tasks.length > 0) {
+        let timer = setInterval(function () {
+            // once all the tasks have ended, clear the timer
+            if (tasks.length == 0) {
+                clearInterval(timer);
+                phase = "idle";
+                // Hide reset button
+                document.getElementById('reset').style.display = 'none';
+            } else {
+                // Display the time MM:SS
+                document.getElementById('timerDisplay').innerHTML = convertSeconds(secondsRemaining);
+                secondsRemaining--;
 
-        document.getElementById('timerDisplay').innerHTML = convertSeconds(secondsRemaining);
-        secondsRemaining--;
-        if (secondsRemaining < 0) {
-            if (phase == "work") {
-                task = tasks.shift();
-            }
-            updatePhase();
-            secondsRemaining = setTimeRemaining();
-        }
-    }, 1000); //update the timer every second
+                if (secondsRemaining < 0) {
+                    if (phase == "work") {
+                        task = tasks.shift();
+                    }
+
+                    updatePhase();
+                    secondsRemaining = setTimeRemaining();
+                }
+           }
+        }, 1000); //update the timer every second
+
+
+    }
+
+
 }
 
 /**
@@ -56,10 +79,26 @@ function convertSeconds(secondsRemaining) {
     return timerString;
 }
 
+/**
+ * Resets the timer and empties the task queue
+ */
+function reset() {
+    tasks = [];
+    phase = 'idle';
+    document.getElementById('timerDisplay').innerHTML='00:00';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('reset').style.display = 'none';
+}
+
 /** 
  * Adds a task to the task queue
  */
 function addTask() {
+    // Load the start button if it's not running
+    if (phase == 'idle') {
+        document.getElementById("start").style.display = 'block';
+    }
+
     tasks.push(document.getElementById("task").value);
     alert('task added');
 }
@@ -90,7 +129,19 @@ function updatePhase() {
  * @return the break time
  */
 function setTimeRemaining() {
-    return (phase == "work") ? 65 :     // work time (seconds)   15 mins (900)
-        (phase == "small_break") ? 30 : // small break time      5 mins  (300)
-            120;                        // big break time        25 mins (1500)
+    return (phase == "work") ? work_length :     
+        (phase == "small_break") ? small_break_length : 
+        big_break_length;                        
+}
+
+function setBigBreak() {
+    big_break_length = document.getElementById("big_break_length").value;
+}
+
+function setSmallBreak() {
+    small_break_length = document.getElementById("small_break_length").value;
+}
+
+function setBigBreak() {
+    work_length = document.getElementById("work_length").value;
 }
