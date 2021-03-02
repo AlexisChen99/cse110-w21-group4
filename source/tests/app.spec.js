90935da6aca01ca1a44f0ee32f1de644038f5876
Elizabeth Cho <elc049@ucsd.edu>
2021-03-01 20:07:40 -0800

    Added Cypress tests and dependencies
    IDs should not be shared (e.g. delete buttons, singleDel)
    Having a count on the task button causes it to shift down
    Input bar should be cleared of text after hitting "Add"

---
+++ source/tests/app.spec.js
@@ -0,0 +1,8 @@
+//const setTimeRemaining = require('../app');
+
+describe('Test setTimeRemaining', () => {
+    test('return short break', () => {
+        //expect(setTimeRemaining()).toBe(5);
+        expect(2).toBe(2);
+    });
+});
