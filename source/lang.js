4c5ca273ee664e6739d9c79083473ec023d4eabe
Miaoqiu Sun <sunmiaoqiu@gmail.com>
2021-03-12 15:51:54 +0800

    added missing Chinese translation

--- source/lang.js
+++ source/lang.js
@@ -182,8 +182,9 @@ var dict = {
                 '[Chinese]\n' + // These lines should be for users who don't know english (see other three areas)
                 '[Spanish]\n' +
                 '[Korean]\n', // 한국어에 설정 :)
-            zh: '' +
-                'To switch to English, click the gear icon to open the settings menu.' + // Translated to something like this 
+            zh: '土豆计时器是一个帮助提高效率的番茄钟定时器，其通过使你专注在特定的工作时间' +
+                '并伴随之后的休息时间。在每四个工作阶段后，将会出现一个长休息时段。\n'+
+                '若想切回英文版，请点击右上角设置图标打开菜单。\n' + // Translated to something like this 
                 '[Spanish]\n' +
                 '[Korean]\n',
             es: '' +
@@ -211,7 +212,12 @@ var dict = {
                 'You can adjust the volume or click on the icon to mute it.\n' +
                 'You can change your language to Español, 中文, 한국어.\n' +
                 '',
-            zh: '',
+            zh: '点击右上角工具图标打开设置菜单。\n'+
+                '随后你将能切换到亮色或暗色模式，以及我们的趣味土豆模式。\n'+
+                '在土豆模式中，如果你觉得被打扰，你可以取消跳舞的土豆动图。\n'+
+                '你可以调整每个周期的长度。\n'+
+                '你可以调整音量或点击图标静音。\n'+
+                '你可以改变语言模式至English, Español，한국어。\n',
             es: '',
             ko: '',
         },
@@ -232,8 +238,15 @@ var dict = {
                 'pin or unpin tasks on the main page, and delete tasks. ' +
                 'If you mark all tasks as done, the timer ends! Well done!\n' + 
                 '*Unfinished tasks are locally stored in your cookies so you can get back to working ' +
-                'If you return to our website!',
-            zh: '',
+                'if you return to our website!',
+            zh: '您可以在开始按钮下白色区域输入任意任务。' +
+                '您可以点击添加图标或按下回车键来添加任务。'+
+                '第一个任务自动添加至主页面。'+
+                '若您想看到全部任务，点击‘任务’按钮。'+
+                '您可以点击圆圈，标记任务为已完成，'+
+                '也可以置顶或取消置顶任务到主页面，以及删除任务。'+
+                '若您标记所有任务为已完成，计时器将自动停止！\n'+
+                '*未完成的任务将自动存储在缓存记录中，以便于您在回到我们的网页时重回工作模式。',
             es: '',
             ko: '',
         },
@@ -247,64 +260,64 @@ var dict = {
         },
         'instrContent': {
             en: 'Potato Timer is happily developed by: \n',
-            zh: '',
+            zh: '土豆计时器由以下成员开发：\n',
             es: '',
             ko: '',
         },
      },
     'enableAnimation': {
         en: 'Enable Potato Animation',
-        zh: '',
+        zh: '启动土豆特效',
         es: '',
         ko: '',
     },
     'disableAnimation': {
         en: 'Disable Potato Animation',
-        zh: '',
+        zh: '取消土豆特效',
         es: '',
         ko: '',
     },
     'notification': {
         'mark': {
             en: 'You marked a task as done!',
-            zh: '',
+            zh: '已标记该任务为完成!',
             es: '',
             ko: '',
         },
         'unmark': {
             en: 'You marked a task as incomplete!',
-            zh: '',
+            zh: '已标记该任务为未完成!',
             es: '',
             ko: '',
         },
         'addTask': {
             en: 'A task has been added!',
-            zh: '',
+            zh: '该任务已添加!',
             es: '',
             ko: '과제를 추가했어!', // Informal :)
             // Uh 하다 / 되다?
         },
         'pinTask': {
             en: 'A task has been pinned!',
-            zh: '',
+            zh: '该任务已放置到主页面!',
             es: '',
             ko: '핀을 추가됐어!', // Pin was added
         },
         'unpinTask': {
             en: 'A task has been unpinned!',
-            zh: '',
+            zh: '该任务已被取消置顶!',
             es: '',
             ko: '핀을 뺐어!', // Pin was removed
         },
         'delTask': {
             en: 'A task has been deleted!',
-            zh: '',
+            zh: '该任务已被删除！',
             es: '',
             ko: '과제를 삭제됐어!', // 되????????
         },
         'deleteAll': {
             en: 'All tasks have been deleted!',
-            zh: '',
+            zh: '所有任务已被删除!',
             es: '',
             ko: '과제를 모두 삭제됐어!',
         },
@@ -312,32 +325,32 @@ var dict = {
     },
     'help': { // aria-label="Show Instructions"
         en: 'Show Instructions',
-        zh: '',
+        zh: '显示帮助菜单',
         es: '',
         ko: '설명은 보여',
     },
     'settingsIcon': { // alt="Show Settings"
         en: 'Show Settings',
-        zh: '',
+        zh: '显示设置菜单',
         es: '',
         ko: '설정은 보여', // Subject particle used because these are topics?
     },
     'mute': { // Mute volume
         en: 'Mute volume', // alt="Mute volume"
-        zh: '',
+        zh: '静音',
         es: '',
         ko: '소리를 숨기다', // Check
     },
     'unmute': {
         en: 'Unmute volume',
-        zh: '',
+        zh: '取消静音',
         es: '',
         ko: '소리를 듣다', // ??????
     },
     'noscript': {
         'sadPotato': { // alt="Sad Potato is Sad"
             en: 'Sad Potato is Sad',
-            zh: '',
+            zh: '土豆很伤心',
             es: '',
             ko: '슬픈 감자',
         },
@@ -345,7 +358,9 @@ var dict = {
             en: 'Oh no!\n' +
             'We\'re sorry, but this website application uses JavaScript to operate the timer.\n' +
             'Please enable JavaScript for the best user experience!',
-            zh: '啊',
+            zh: '哦不！\n'+
+            '抱歉，改网站使用JavaScript来运行计时器。\n'+
+            '请启动JavaScript以享受最佳使用感受！',
             es: '',
             ko: '어머! ', 
         },
@@ -353,25 +368,25 @@ var dict = {
 
     'markBtn': { // ARIA Screen Reader aria-label="Mark as done"
         en: 'Mark as Done',
-        zh: '',
+        zh: '标记为已完成',
         es: '',
         ko: '',
     },
     'pinBtn': { // ARIA Screen Reader 
         en: 'Pin to the Main Display',
-        zh: '',
+        zh: '添加到主页面展示',
         es: '',
         ko: '',
     },
     'delBtn': { // ARIA Screen Reader
         en: 'Delete this Task',
-        zh: '',
+        zh: '删除该任务',
         es: '',
         ko: '',
     },
     'taskHelp': {
         en: 'Mark a task as complete. Pin a task to the main display.',
-        zh: '',
+        zh: '标记任务为已完成，或添加任务到主页面。',
         es: '',
         ko: '',
     },
